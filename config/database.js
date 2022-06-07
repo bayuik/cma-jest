a199d22ff0cfb09d173b431e50e26715796480dd
bayuik <bayuindrakusuma05@gmail.com>
2022-06-07 08:25:29 +0700

    update db name

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const {
   DB_USER = "agtksrlvwcstee",
   DB_PASSWORD = "8dc1b440c8f1ac251a09ae8929893b9b9a99c267cb00ef74ec949b4ddc394b6c",
-  DB_NAME = "car",
+  DB_NAME = "d2litmspul4gdr",
   DB_HOST = "ec2-52-204-195-41.compute-1.amazonaws.com",
   DB_PORT = "5432",
 } = process.env;
